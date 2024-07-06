9ab3446e6c4335f968caad64bb6b0a151ee53d60
Mathis Verstrepen <mathisverstrepen@gmail.com>
2024-07-06 10:11:08 +0200

    🎉 HTMX + Templ template first version

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"diikstra.fr/template/handlers"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+		Level: 5,
+	}))
+	e.Static("/assets", "assets")
+
+	handlers.Init()
+
+	// ---- Home Routes ---- //
+	e.GET("/", handlers.HomeHandler)
+
+	// ---- Global Routes ---- //
+	e.GET("/ping", handlers.GlobalPing)
+	if os.Getenv("ENV") != "prod" {
+		e.GET("/ws", handlers.GlobalHotReloadWS)
+	}
+
+	e.Logger.Fatal(e.Start(":42069"))
+}
